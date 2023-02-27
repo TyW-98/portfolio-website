96cc2b2c1691eaa69bbd8a0ae83162a1e507dd72
wey1998 <yitwey1998@hotmail.com>
2023-02-27 17:30:59 +0000

    Implementing typing animation for welcome title message

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
+// Open overlay navigation menu
 function openNav() {
     document.getElementById("overlayNav").style.height = "100%";
 }
 
+// Close overlay navigation menu
 function closeNav() {
     document.getElementById("overlayNav").style.height = "0%";
 }
+
+// The length of the title message
+var welcomeMsg = document.querySelector(".welcome-message")
+var welcomeMsgLength = welcomeMsg.textContent.length
+
+document.documentElement.style.setProperty('--step','welcomeMsgLength');
