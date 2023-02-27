a65e913ac4677fb65de916d64f501cbba07ab846
wey1998 <yitwey1998@hotmail.com>
2023-02-27 21:43:52 +0000

    Add slide left animation to overlay navigation

--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
 // Open overlay navigation menu
 function openNav() {
-    document.getElementById("overlayNav").style.height = "100%";
+    // Get the overlay navigation element
+    var overlayNav = document.getElementById("overlayNav");
+  
+    // Move the overlay navigation from the left to the center of the screen
+    overlayNav.style.left = "0";
 }
 
 // Close overlay navigation menu
 function closeNav() {
-    document.getElementById("overlayNav").style.height = "0%";
+    // Get the overlay navigation element
+    var overlayNav = document.getElementById("overlayNav");
+  
+    // Move the overlay navigation back to the left side of the screen
+    overlayNav.style.left = "-1000%";
 }
 
 // The length of the title message
