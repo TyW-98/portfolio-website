4fd2784d5936cfcdfc88cd90e0c051e51693de61
wey1998 <yitwey1998@hotmail.com>
2023-02-28 16:41:16 +0000

    Fixed open overlay navigation to top right of screen and move with scroll

--- script/index.js
+++ script/index.js
@@ -2,9 +2,16 @@
 function openNav() {
     // Get the overlay navigation element
     var overlayNav = document.getElementById("overlayNav");
-  
+    
     // Move the overlay navigation from the left to the center of the screen
     overlayNav.style.left = "0";
+
+    // Get open overlay navigation button element
+    var openBtn = document.querySelector(".openbtn"); 
+
+    // Set the visibility of the button to hidden when the overlay navigation is opened.
+    openBtn.style.visibility = "hidden";
+
 }
 
 // Close overlay navigation menu
@@ -14,6 +21,12 @@ function closeNav() {
 
   // Move the overlay navigation back to the left side of the screen
   overlayNav.style.left = "-1000%";
+
+  // Get open overlay navigation button element 
+  var openBtn = document.querySelector(".openbtn"); 
+
+  // Set the visibility of the button to show when the overlay navigation is closed. 
+  openBtn.style.visibility = "visible";
 }
 
 // The length of sub title 
