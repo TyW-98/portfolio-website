6bd081081958ef0b7b132b7c71cbdadfbd89e313
wey1998 <yitwey1998@hotmail.com>
2023-04-29 05:08:03 +0100

    Add animation to home page

--- public/script/index.js
+++ public/script/index.js
@@ -1,7 +1,11 @@
 const Typewriter = window.Typewriter;
 
+const console = document.getElementsByClassName("console")[0];
 const introHeading = document.getElementsByClassName("console-text")[0];
 const newConsoleRow = document.getElementsByClassName("new-console-row")[0];
+const footer = document.getElementById("footer");
+const header = document.getElementsByClassName("navbar")[0];
+const welMsg = document.getElementsByClassName("welcome-message")[0];
 
 const typewriter = new Typewriter(introHeading, {
   autoStart: true,
@@ -22,4 +26,11 @@ consoleTyping().then(() => {
   const cursor = document.getElementsByClassName("Typewriter__cursor")[0];
   cursor.innerHTML = "";
   newConsoleRow.style.visibility = "visible";
+  setTimeout(() => {
+    console.classList.add("hide");
+    newConsoleRow.innerHTML = "";
+    footer.classList.add("show");
+    header.classList.add("show");
+    welMsg.classList.add("show");
+  }, 1500);
 });
